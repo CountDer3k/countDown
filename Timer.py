34b4a8d34b176d1d69e6cc8e9963e8888157a3e7
Count Der3k <der3kburrola@gmail.com>
2020-08-01 15:49:31 -0600

    Added support for hours, minutes, and seconds together

--- Timer.py
+++ Timer.py
@@ -4,10 +4,8 @@ import emoji
 
 ALARM_CLOCK = emoji.emojize(':alarm_clock:')
 ALARM_CLOCK_2 = ALARM_CLOCK + ALARM_CLOCK
-ALARM_CLOCK_3 = ALARM_CLOCK + ALARM_CLOCK + ALARM_CLOCK
 ALARM_CLOCK_8 = ALARM_CLOCK_2 + ALARM_CLOCK_2 + ALARM_CLOCK_2 + ALARM_CLOCK
 TAB_10 = '\t\t'
-#TAB_12 = TAB_10 + TAB_10#
 MINUTES_IN_HOUR = 60
 
 def countDown_finished():
@@ -52,72 +50,85 @@ def clockEmoji():
 
 
 
-
 def intput(x):
-	m = ''
-	s = ''
-	time = []
-	splitFound = False
-	if('.' in x):
-		for i in range (len(x)):
-			if(x[i] == '.'):
-				splitFound = True
-			if(not splitFound):
-				m += x[i]
-			else:
-				if(x[i] != '.'):
-					s += x[i]
-		time.append(int(m))
-		time.append(int(s))
-	else:
-		time.append(int(x))
-		time.append(0)
-	return time
+	h = 0
+	m = 0
+	s = 0
+	timeList = []
+	if(len(x) == 8):
+		# hours + min + sec
+		h = int(x[0] + x[1])
+		m = int(x[3] + x[4])
+		s = int(x[6] + x[7])
+	if(len(x) == 5):
+		# minutes and seconds
+		m = int(x[0] + x[1])
+		s = int(x[3] + x[4])
+	if(len(x) == 3):
+		# seconds only
+		s = int(x[1] + x[2])
+	if(len(x) == 2):
+		# Minutes only
+		m = int(x[0] + x[1])
+	timeList.append(h)
+	timeList.append(m)
+	timeList.append(s)
+	return timeList
 
-def countDown(minutes, seconds):
-	t = (minutes * MINUTES_IN_HOUR) + seconds
+
+def countDown(hours, minutes, seconds):
+	t = (hours * MINUTES_IN_HOUR * MINUTES_IN_HOUR) + (minutes * MINUTES_IN_HOUR) + seconds
 	totalTime = str(minutes) + ':' + str(seconds)
 	while t:
 		os.system('clear')
 		mins, secs = divmod(t, MINUTES_IN_HOUR)
-		timeLeft = '{:02d}:{:02d}'.format(mins, secs)
+		hour, mins = divmod(mins, MINUTES_IN_HOUR)
+		timeLeft = '{:02d}:{:02d}:{:02d}'.format(hour, mins, secs)
 		print(ALARM_CLOCK + ' Timer is set for: ' + str(totalTime) + ' minutes ' + ALARM_CLOCK)
 		print(timeLeft, end='\n')
 		time.sleep(1)
 		t -= 1
 	countDown_finished()
 
+def showHelp():
+	print('\n########################################################')
+	print('Insturctions:')
+	print('Separated time with \'.\' between hours, minutes & seconds')
+	print('Example for 1 hour 10 minutes with 45 seconds: 01.10.45')
+	print('For second only do: .XX ')
+	print('For minutes only do: XX')
+	print('Requires 2 digits per area.')
+	print('\n########################################################')
+	prepareInput()
 
-
-print('\n########################################################')
-print('Insturctions:')
-print('Enter amount of minutes separated by seconds with a \'.\'')
-print('Example for 10 minutes with 45 seconds: 10.45')
-print('For second only do: .XX ')
-print('For minutes only do: XX')
-print('########################################################')
-print('\nInput time: ')
-
-
-inputTime = input()
-minutes = intput(inputTime)[0]
-seconds = intput(inputTime)[1]
-while(minutes > 60 or seconds > 60):
-	print('Minutes and seconds must be less than 60 each \n Input time: ')
-	time = input()
-	minutes = intput(time)[0]
-	seconds = intput(time)[1]
-countDown(minutes,seconds)
-
-
-if(False):
+def prepareInput():
+	print('\nInput time: ')
 	inputTime = input()
-	while(inputTime > 60):
-		print('Input time (in minutes - less than 60min): ')
-		inputTime = input()
-	countDown(inputTime)
-	#countDown(inputTime)
+	if(inputTime == 'help'):
+		showHelp()
+	else:
+		hours = intput(inputTime)[0]
+		minutes = intput(inputTime)[1]
+		seconds = intput(inputTime)[2]
+		while(minutes > 60 or seconds > 60):
+			print('Minutes and seconds must be less than 60 each \n Input time: ')
+			time = input()
+			hours = intput(inputTime)[0]
+			minutes = intput(inputTime)[1]
+			seconds = intput(inputTime)[2]
+		countDown(hours, minutes, seconds)
 
+def main():
+	print('\n########################################################')
+	print('Insturctions:')
+	print('Enter time separated by \'.\' between hours, minutes & seconds')
+	print('type \'help\' for more info')
+	print('########################################################')
+	prepareInput()
+	
+
+if __name__ == '__main__':
+    main()
 
 
 
